b74e1f1b6116723cec61030e78b5271d94fb7c89
CKHuang-public <chihkaihuang1@gmail.com>
2025-02-11 00:03:04 +0100

    rearrange

--- clusters_deployment/scenario2/03_system_ready.sh
+++ clusters_deployment/scenario2/03_system_ready.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 i=0
 manage=$(awk NR==1 node_list)
-git clone https://github.com/satrai-lab/edgesys-2025
+git clone https://github.com/CKHuangGH/edgesys-2025
 rm -rf /home/chuang/.ssh/known_hosts
 
 for j in $(cat node_list)
@@ -13,6 +13,7 @@ scp /home/chuang/kubectl-karmada root@$j:/usr/local/bin/kubectl-karmada &
 
 scp /home/chuang/.ssh/id_rsa root@$j:/root/.ssh &
 done
+
 sleep 20
 
 for j in $(cat node_list)
@@ -22,7 +23,10 @@ ssh -o StrictHostKeyChecking=no root@$j chmod 777 -R /root/edgesys-2025/
 i=$((i+1))
 done
 
-scp node_list root@$manage:/root/edgesys-2025/federation_framework/ocm/node_list
-scp node_list root@$manage:/root/edgesys-2025/federation_framework/karmada-pull/node_list
-scp node_list root@$manage:/root/edgesys-2025/federation_framework/karmada-push/node_list
+scp node_list root@$manage:/root/edgesys-2025/federation_framework/scenario1/ocm/node_list
+scp node_list root@$manage:/root/edgesys-2025/federation_framework/scenario1/karmada-pull/node_list
+scp node_list root@$manage:/root/edgesys-2025/federation_framework/scenario1/karmada-push/node_list
+scp node_list root@$manage:/root/edgesys-2025/federation_framework/scenario2/ocm/node_list
+scp node_list root@$manage:/root/edgesys-2025/federation_framework/scenario2/karmada-pull/node_list
+scp node_list root@$manage:/root/edgesys-2025/federation_framework/scenario2/karmada-push/node_list
 echo "management node is $manage"
