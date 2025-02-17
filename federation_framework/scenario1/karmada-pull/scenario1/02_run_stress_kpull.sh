27e03e8b64f3e19d9ae5da87da56b9a24ce58162
CKHuang-public <chihkaihuang1@gmail.com>
2025-02-17 20:40:59 +0100

    pull go

--- federation_framework/scenario1/karmada-pull/scenario1/02_run_stress_kpull.sh
+++ federation_framework/scenario1/karmada-pull/scenario1/02_run_stress_kpull.sh
@@ -14,8 +14,8 @@ echo "start deployment" >> number.txt
 echo $(date +'%s.%N') >> number.txt
 . ./script/$number.sh > /dev/null 2>&1 &
 
-# . ./checking_deployment_kpull.sh $number &
-# . ./checking_kpull.sh $number
+. ./checking_deployment_kpull.sh $number &
+. ./checking_kpull.sh $number
 
 exec -a tophub bash -c "./script/tophub.sh" > /dev/null 2>&1 &
 
@@ -27,7 +27,7 @@ done
 sudo tcpdump -i ens3 -nn -q '(src net 10.176.0.0/16 and dst net 10.176.0.0/16) and not arp' >> cross > /dev/null 2>&1 &
 
 echo "wait for 900 secs"
-for (( i=9; i>0; i-- )); do
+for (( i=900; i>0; i-- )); do
     echo -ne "\r$i secs remaining..."
     sleep 1
 done
