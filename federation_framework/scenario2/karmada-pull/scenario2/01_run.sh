903269d415c49fe26f8a912e843730a6706df668
CKHuang-public <chihkaihuang1@gmail.com>
2025-02-23 00:27:25 +0100

    test

--- federation_framework/scenario2/karmada-pull/scenario2/01_run.sh
+++ federation_framework/scenario2/karmada-pull/scenario2/01_run.sh
@@ -2,16 +2,16 @@ read -p "please enter the test number(1, 50, 100, 150, 200): " number
 
 for (( times=0; times<10; times++ )); do
     . ./init_reg.sh
-    sleep 40
+    sleep 30
     mkdir results
     . ./02_run_stress_kpull.sh $number
     . ./03.getdocker.sh $number $times
-    sleep 20
+    sleep 30
     for ip in $(cat node_exec)
     do 
 	    ssh root@$ip . /root/edgesys-2025/federation_framework/scenario2/karmada-pull/scenario2/reset_worker.sh
     done
     . ./reset.sh $number
     rm -rf results
-    sleep 60
+    sleep 30
 done
