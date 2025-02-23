c25c0d77557a8d55bf520a886164916c3b4327bb
CKHuang-public <chihkaihuang1@gmail.com>
2025-02-23 22:22:15 +0100

    wsws

--- federation_framework/scenario2/karmada-pull/scenario2/02_run_stress_kpull.sh
+++ federation_framework/scenario2/karmada-pull/scenario2/02_run_stress_kpull.sh
@@ -1,17 +1,21 @@
 number=$1
-
+clusternumber=1
 while read -r ip; do
     if [[ "$ip" =~ ^[[:space:]]*$ || "$ip" =~ ^\s*# ]]; then
         continue
     fi
-
-    ping -c 2 "$ip" > number.txt
+    echo "=== Cluster #$clusternumber ($ip) ===" >> number.txt
+    ping -c 2 "$ip" >> number.txt
+    kubectl --context "cluster$clusternumber" get pod -A >> number.txt
+    ((clusternumber++))
 done < "node_list"
 
 echo $number
 echo $number >> number.txt
 echo "start deployment" >> number.txt
 echo $(date +'%s.%N') >> number.txt
+
+kubectl --context cluster0 get pod -A >> number.txt
 
 exec -a tophub bash -c "./script/tophub.sh" > /dev/null 2>&1 &
 
--- federation_framework/scenario2/ocm/scenario2/02_run_stress_ocm.sh
+++ federation_framework/scenario2/ocm/scenario2/02_run_stress_ocm.sh
@@ -1,10 +1,13 @@
 number=$1
-
+clusternumber=1
 while read -r ip; do
     if [[ "$ip" =~ ^[[:space:]]*$ || "$ip" =~ ^\s*# ]]; then
         continue
     fi
-    ping -c 2 "$ip" > number.txt
+    echo "=== Cluster #$clusternumber ($ip) ===" >> number.txt
+    ping -c 2 "$ip" >> number.txt
+    kubectl --context "cluster$clusternumber" get pod -A >> number.txt
+    ((clusternumber++))
 done < "node_list"
 
 echo $number
@@ -12,9 +15,11 @@ echo $number >> number.txt
 echo "start deployment" >> number.txt
 echo $(date +'%s.%N') >> number.txt
 
+kubectl --context cluster0 get pod -A >> number.txt
+
 exec -a tophub bash -c "./script/tophub.sh" > /dev/null 2>&1 &
 
-sudo tcpdump -i ens3 -nn -q '(src net 10.176.0.0/16 and dst net 10.176.0.0/16) and not arp' >> cross > /dev/null 2>&1 &
+sudo tcpdump -i ens3 -nn -q '(src net 10.176.0.0/16 and dst net 10.176.0.0/16) and not arp' >> cross &
 
 echo "wait for 900 secs"
 for (( i=900; i>0; i-- )); do
