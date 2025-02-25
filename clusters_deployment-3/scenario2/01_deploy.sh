d2af49e60128a4d607134873be3b67278f726b02
CKHuangGH <chihkaihuang1@gmail.com>
2025-02-25 17:57:29 +0100

    sddd

--- clusters_deployment-3/scenario2/01_deploy.sh
+++ clusters_deployment-3/scenario2/01_deploy.sh
@@ -4,17 +4,17 @@ python3 ./cluster/m1.py &
 python3 ./cluster/m2.py &
 python3 ./cluster/m3.py &
 python3 ./cluster/m4.py &
-python3 ./cluster/m5.py
-# python3 ./cluster/m6.py &
-# python3 ./cluster/m7.py &
-# python3 ./cluster/m8.py &
-# python3 ./cluster/m9.py &
-# python3 ./cluster/m10.py &
-# python3 ./cluster/m11.py &
-# python3 ./cluster/m12.py &
-# python3 ./cluster/m13.py &
-# python3 ./cluster/m14.py &
-# python3 ./cluster/m15.py &
+python3 ./cluster/m5.py &
+python3 ./cluster/m6.py &
+python3 ./cluster/m7.py &
+python3 ./cluster/m8.py &
+python3 ./cluster/m9.py &
+python3 ./cluster/m10.py &
+python3 ./cluster/m11.py &
+python3 ./cluster/m12.py &
+python3 ./cluster/m13.py &
+python3 ./cluster/m14.py &
+python3 ./cluster/m15.py
 # python3 ./cluster/m16.py &
 # python3 ./cluster/m17.py &
 # python3 ./cluster/m18.py &
@@ -26,7 +26,7 @@ chmod 777 03_system_ready.sh
 chmod 777 04_del.sh
 
 echo "wait for 600 secs"
-for (( i=180; i>0; i-- )); do
+for (( i=240; i>0; i-- )); do
     echo "$i secs..."
     sleep 1
 done
