555ed818fccef5667ca23e401834dff5de77b23e
CKHuangGH <chihkaihuang1@gmail.com>
2025-02-26 17:11:48 +0100

    Update 01_deploy.sh

--- clusters_deployment/scenario2/01_deploy.sh
+++ clusters_deployment/scenario2/01_deploy.sh
@@ -4,12 +4,12 @@ python3 ./cluster/m1.py &
 python3 ./cluster/m2.py &
 python3 ./cluster/m3.py &
 python3 ./cluster/m4.py &
-python3 ./cluster/m5.py
-# python3 ./cluster/m6.py &
-# python3 ./cluster/m7.py &
-# python3 ./cluster/m8.py &
-# python3 ./cluster/m9.py &
-# python3 ./cluster/m10.py &
+python3 ./cluster/m5.py &
+python3 ./cluster/m6.py &
+python3 ./cluster/m7.py &
+python3 ./cluster/m8.py &
+python3 ./cluster/m9.py &
+python3 ./cluster/m10.py
 # python3 ./cluster/m11.py &
 # python3 ./cluster/m12.py &
 # python3 ./cluster/m13.py &
@@ -26,7 +26,7 @@ chmod 777 03_system_ready.sh
 chmod 777 04_del.sh
 
 echo "wait for 600 secs"
-for (( i=300; i>0; i-- )); do
+for (( i=600; i>0; i-- )); do
     echo "$i secs..."
     sleep 1
 done
